29a90d022c26afbe2922d6ad421beb2a7aeda9e6
Fredrik Emilsson <fredrik.emilsson@live.se>
2014-07-22 21:48:18 +0200

    Updated

--- ch02.py
+++ ch02.py
@@ -15,32 +15,35 @@ print records[0]['tz']
 time_zones = [rec['tz'] for rec in records if 'tz' in rec]
 time_zones[:10]
 
+
 def get_counts(sequence):
-	counts = {}	
-	for x in sequence:
-		if x in counts:
-			counts[x] += 1
-		else:
-			counts[x] = 1	
-	return counts
-	
+    counts = {}
+    for x in sequence:
+        if x in counts:
+            counts[x] += 1
+        else:
+            counts[x] = 1
+    return counts
+
 from collections import defaultdict
 
+
 def get_counts2(sequence):
-	counts = defaultdict(int) # values will initialize to 0
-	for x in sequence:
-		counts[x] += 1
-	return counts
-	
+    counts = defaultdict(int) # values will initialize to 0
+    for x in sequence:
+        counts[x] += 1
+    return counts
+
 counts = get_counts(time_zones)
 counts['America/New_York']
 len(time_zones)
 
+
 def top_counts(count_dict, n=10):
-	value_key_pairs = [(count, tz) for tz, count in count_dict.items()]
-	value_key_pairs.sort()
-	return value_key_pairs[-n:]
-	
+    value_key_pairs = [(count, tz) for tz, count in count_dict.items()]
+    value_key_pairs.sort()
+    return value_key_pairs[-n:]
+
 top_counts(counts)
 
 from collections import Counter
@@ -60,7 +63,9 @@ clean_tz[clean_tz == ''] = 'Unknown'
 tz_counts = clean_tz.value_counts()
 tz_counts[:10]
 
+import matplotlib.pyplot as plt
 tz_counts[:10].plot(kind='barh', rot=0)
+plt.show()
 
 frame['a'][1]
 frame['a'][50]
@@ -71,6 +76,7 @@ results = Series([x.split()[0] for x in frame.a.dropna()])
 results[:5]
 results.value_counts()[:8]
 
+import numpy as np
 cframe = frame[frame.a.notnull()]
 operating_system = np.where(cframe['a'].str.contains('Windows'),'Windows', 'Not Windows')
 operating_system[:5]
@@ -84,4 +90,8 @@ count_subset = agg_counts.take(indexer)[-10:]
 count_subset
 
 count_subset.plot(kind='barh', stacked=True)
+plt.show()
+
 normed_subset = count_subset.div(count_subset.sum(1), axis=0)
+normed_subset.plot(kind='barh', stacked=True)
+plt.show()
