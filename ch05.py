3fff303ea74fdedb02d8ea64330a30c827ef55c5
Fredrik Emilsson <fredrik.emilsson@live.se>
2014-08-01 22:45:43 +0200

    update

--- ch05.py
+++ ch05.py
@@ -111,3 +111,39 @@ obj2.index is index
 frame3
 'Ohio' in frame3.columns
 2003 in frame3.index
+
+obj = Series([4.5, 7.2, -5.3, 3.6], index=['d', 'b', 'a', 'c'])
+obj
+
+obj2 = obj.reindex(['a', 'b', 'c', 'd', 'e'])
+obj2
+obj.reindex(['a', 'b', 'c', 'd', 'e'], fill_value=0)
+
+obj3 = Series(['blue', 'purple', 'yellow'], index=[0, 2, 4])
+obj3
+obj3.reindex(range(6), method='ffill')
+
+frame = DataFrame(np.arange(9).reshape((3, 3)), index=['a', 'c', 'd'],
+                  columns=['Ohio', 'Texas', 'California'])
+frame
+frame2 = frame.reindex(['a', 'b', 'c', 'd'])
+frame2
+states = ['Texas', 'Utah', 'California']
+frame.reindex(columns=states)
+frame.reindex(index=['a', 'b', 'c', 'd'], method='ffill',
+              columns=states)
+frame.ix[['a', 'b', 'c', 'd'], states]
+
+obj = Series(np.arange(5.), index=['a', 'b', 'c', 'd', 'e'])
+obj
+new_obj = obj.drop('c')
+new_obj
+obj.drop(['d', 'c'])
+
+data = DataFrame(np.arange(16).reshape((4, 4)),
+                 index=['Ohio', 'Colorado', 'Utah', 'New York'],
+                 columns=['one', 'two', 'three', 'four'])
+data
+data.drop(['Colorado', 'Ohio'])
+data.drop('two', axis=1)
+data.drop(['two', 'four'], axis=1)
